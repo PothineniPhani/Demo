570858833ce030bc8d2e89227a77f88a56594b6d
PothineniPhani <phani.pothineni@gmail.com>
2021-05-11 20:12:05 +0530

    FirstCommit

---
+++ src/test/java/DemoApp/DemoApp/AppTest.java
@@ -0,0 +1,18 @@
+package DemoApp.DemoApp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testApp() {
+
+	}
+
+	@Test
+	public void testApp1() {
+		Assert.fail();
+	}
+
+}
